fc0209521da1e9a373080c356843e483dfc9b61c
ehijie11 <145166879+ehijie11@users.noreply.github.com>
2024-04-26 03:25:39 +0100

    new

---
+++ examples/if_only.py
@@ -0,0 +1,21 @@
+first_number = int(input("What is your First number? "))
+second_number = int(input("What is your Second number? "))
+
+#conditions
+if first_number > second_number:
+    print("Your first number is greater")
+else :
+    print("Your first number is not greater")
+
+if second_number > first_number:
+        print("Your second number is greater")
+else :
+        print("Your second number is not greater")
+        
+if first_number == second_number:
+        print("The numbers are Equal")
+else :
+        print("the numbers are not Equal")    
+    
+
+        
